2db09bd50dca8617795b29e9070cfe86901232d3
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-04-02 19:19:41 +0530

    database added

---
+++ server/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+	const connectionParams = {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	};
+	try {
+		mongoose.connect('mongodb://127.0.0.1:27017/zoomapi2', connectionParams);
+		console.log("Connected to database successfully");
+	} catch (error) {
+		console.log(error);
+		console.log("Could not connect database!");
+	}
+};
