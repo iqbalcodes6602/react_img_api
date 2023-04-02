f3caf57cbb2079445b2f76eff432a5be5f5fddc0
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-04-02 19:20:41 +0530

    frontend files

---
+++ client/src/components/Main/index.jsx
@@ -0,0 +1,24 @@
+import styles from "./styles.module.css";
+
+const Main = () => {
+	const handleLogout = () => {
+		localStorage.removeItem("token");
+		window.location.reload();
+	};
+
+	return (
+		<div className={styles.main_container}>
+			<nav className={styles.navbar}>
+				<h1>fakebook</h1>
+				<button className={styles.white_btn} onClick={handleLogout}>
+					Logout
+				</button>
+			</nav>
+			<div>
+				hello there this is the dashboard
+			</div>
+		</div>
+	);
+};
+
+export default Main;
