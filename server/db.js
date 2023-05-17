78c5c426afe420d2e7610794512c073fe1c26fcd
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-05-17 18:39:51 +0530

    db updated

--- server/db.js
+++ server/db.js
@@ -7,7 +7,7 @@ module.exports = () => {
 	};
 	try {
 		// mongoose.connect('mongodb+srv://mohdabd6602:kyFGuzJ5S4v95gkZ@cluster0.sokktjd.mongodb.net/', connectionParams);
-		mongoose.connect('mongodb+srv://mohdabd6602:kyFGuzJ5S4v95gkZ@cluster0.sokktjd.mongodb.net/test?retryWrites=true&w=majority', connectionParams);
+		mongoose.connect('mongodb+srv://iqbal:qJiI1unxi6Ih9eql@cluster0.sp50fhr.mongodb.net/hubxdb?retryWrites=true&w=majority', connectionParams);
 		
 		// mongoose.connect('mongodb://127.0.0.1:27017/hubx', connectionParams);
 		console.log("Connected to database successfully");
