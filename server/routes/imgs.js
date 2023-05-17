7c7e086d53eed2c38b9581195b71afe390cfee52
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-05-17 10:45:25 +0530

    routes for images

--- server/routes/imgs.js
+++ server/routes/imgs.js
@@ -3,28 +3,17 @@ const Image = require("../models/img");
 
 router.post("/upload", async (req, res) => {
 	try {
-		const { error } = validate(req.body);
-		if (error)
-			return res.status(400).send({ message: error.details[0].message });
-
-		// const user = await User.findOne({ email: req.body.email });
-		// if (user)
-		// 	return res
-		// 		.status(409)
-		// 		.send({ message: "User with given email already Exist!" });
-
-		// const salt = await bcrypt.genSalt(Number("hubx"));
-		// const hashPassword = await bcrypt.hash(req.body.password, salt);
-
 		await new Image({
 			user: req.body.user,
 			title: req.body.title,
 			description: req.body.description,
+			cloudinaryUrl: req.body.cloudinaryUrl,
 			viewsCount: req.body.viewsCount
 		}).save();
+
 		res.status(201).send({ message: "Image uploaded successfully" });
 	} catch (error) {
-		res.status(500).send({ message: "Internal Server Error" });
+		res.status(500).send({ message: "Internal Server Error" + error.message });
 	}
 });
 
