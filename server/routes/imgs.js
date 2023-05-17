575bd0223e3cfa7fafcb68877305ddac209c61b8
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-05-17 14:09:19 +0530

    view button bacedn

--- server/routes/imgs.js
+++ server/routes/imgs.js
@@ -18,14 +18,30 @@ router.post("/upload", async (req, res) => {
 	}
 });
 
-router.get("/images", async (req, res) => {
+router.get("/images/:userId", async (req, res) => {
 	try {
-		const images = await Image.find();
+		const userId = req.params.userId;
+		const images = await Image.find({ user: userId });
 		res.status(200).send(images);
 	} catch (error) {
 		res.status(500).send({ message: "Internal Server Error" });
 	}
 });
 
+router.post("/:id/view", async (req, res) => {
+	try {
+		const image = await Image.findById(req.params.id);
+		if (!image) {
+			return res.status(404).send({ message: "Image not found" });
+		}
+
+		image.viewsCount += 1;
+		await image.save();
+
+		res.status(200).send({ message: "Image view count updated" });
+	} catch (error) {
+		res.status(500).send({ message: "Internal Server Error" });
+	}
+});
 
 module.exports = router;
