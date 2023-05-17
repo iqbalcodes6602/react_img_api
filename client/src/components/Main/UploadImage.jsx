e4ce3b37112a47dc39794bd4b68ecc4c3a254adb
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-05-17 12:24:24 +0530

    upload image to cloud

--- client/src/components/Main/UploadImage.jsx
+++ client/src/components/Main/UploadImage.jsx
@@ -22,10 +22,6 @@ const UploadImage = () => {
     const [cloudinaryUrl, setCloudinaryUrl] = useState('');
     const [image, setImage] = useState(null);
 
-    // const handleUserChange = (e) => {
-    //     setUser(e.target.value);
-    // };
-
     const handleTitleChange = (e) => {
         setTitle(e.target.value);
     };
@@ -53,12 +49,45 @@ const UploadImage = () => {
 
             const response = await axios.post('http://localhost:8080/api/img/upload', requestBody);
 
-            console.log(userId);
             console.log(response.data); // Log the response from the backend
         } catch (error) {
             console.error(error);
         }
     };
+
+
+    const postDetails = (pics) => {
+        if (!pics || pics.length === 0) {
+            window.alert("Please select an image");
+            return;
+        }
+
+        const file = pics[0];
+        if (file.type === "image/jpeg" || file.type === "image/png") {
+            const data = new FormData();
+            data.append("upload_preset", "wkvvcgfu");
+            data.append("file", file);
+            data.append("cloud_name", "dcqiakni6");
+            fetch("https://api.cloudinary.com/v1_1/dcqiakni6/image/upload", {
+                method: "post",
+                body: data,
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    setCloudinaryUrl(data.url.toString());
+                    console.log(cloudinaryUrl);
+                    console.log(data.url.toString());
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        } else {
+            window.alert("Please select a valid image (JPEG or PNG)");
+            return;
+        }
+    };
+
+
 
 
     return (
@@ -69,27 +98,45 @@ const UploadImage = () => {
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <div>
-                    <div>
-                        <label>Title:</label>
-                        <input type="text" value={title} onChange={handleTitleChange} />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label htmlFor="title">Title:</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="title"
+                                    value={title}
+                                    onChange={handleTitleChange}
+                                />
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label htmlFor="description">Description:</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="description"
+                                    value={description}
+                                    onChange={handleDescriptionChange}
+                                />
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <label>Description:</label>
-                        <input type="text" value={description} onChange={handleDescriptionChange} />
-                    </div>
-                    <div>
-                        <label>URL:</label>
-                        <input type="text" value={cloudinaryUrl} onChange={handleCloudinaryUrlChange} />
-                    </div>
-                    <button onClick={handleUpload}>Upload</button>
                 </div>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>
                     <div>
                         <label>Image:</label>
-                        <input type="file" accept="image/*" onChange={handleImageChange} />
+                        <input
+                            placeholder='Image'
+                            type="file"
+                            accept="image/"
+                            onChange={(e) => { postDetails(e.target.files) }}
+                        />
                     </div>
                 </Button>
                 <Button variant="primary" onClick={handleUpload}>
@@ -101,3 +148,7 @@ const UploadImage = () => {
 };
 
 export default UploadImage;
+
+
+
+
