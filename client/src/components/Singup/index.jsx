33ecb4e2ede4a3f68aaa47436fada28fa9106123
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-05-17 16:09:18 +0530

    sign up

--- client/src/components/Singup/index.jsx
+++ client/src/components/Singup/index.jsx
@@ -42,7 +42,7 @@ const Signup = () => {
 					<h1>Welcome Back</h1>
 					<Link to="/login">
 						<button type="button" className={styles.white_btn}>
-							Sing in
+							Sign in
 						</button>
 					</Link>
 				</div>
