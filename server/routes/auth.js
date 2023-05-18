3042ca301c3bbe026091269ae264a7bf4a9721ad
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-05-18 10:58:38 +0530

    error msgs

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -11,14 +11,14 @@ router.post("/", async (req, res) => {
 
 		const user = await User.findOne({ email: req.body.email });
 		if (!user)
-			return res.status(401).send({ message: "Invalid Email or Password" });
+			return res.status(401).send({ message: "No Email Found" });
 
 		const validPassword = await bcrypt.compare(
 			req.body.password,
 			user.password
 		);
 		if (!validPassword)
-			return res.status(401).send({ message: "Invalid Email or Password" });
+			return res.status(401).send({ message: "Invalid Password" });
 
 		const token = user.generateAuthToken();
 		res.status(200).send({ data: token, message: "logged in successfully" });
