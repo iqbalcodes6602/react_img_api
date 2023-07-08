0b0cf9251fb49b72fff3bfb5feb50180310da148
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-07-08 19:53:21 +0530

    db saved

--- server/db.js
+++ server/db.js
@@ -6,8 +6,7 @@ module.exports = () => {
 		useUnifiedTopology: true,
 	};
 	try {
-		mongoose.connect('mongodb+srv://iqbal:IiCasop5x31BuEX2@cloudpix-cluster.r2r45sf.mongodb.net/?retryWrites=true&w=majority', connectionParams);
-		// mongoose.connect('mongodb://127.0.0.1:27017/hubx', connectionParams);
+		mongoose.connect('mongodb://127.0.0.1:27017/hubx', connectionParams);
 		console.log("Connected to database successfully");
 	} catch (error) {
 		console.log(error);
